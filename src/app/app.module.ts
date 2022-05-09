a2063139615f9f09024aa155ea0c15037c8025a3
Pedro Chalegre <pedrochalegre7@gmail.com>
2022-05-09 17:00:03 -0300

    feat(fale conosco): component fale conosco adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
 
@@ -11,6 +12,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   declarations: [
     AppComponent,
     NavbarComponent,
+    ContactComponent,
     
   
   ],
