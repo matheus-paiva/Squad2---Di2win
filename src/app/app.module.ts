9f170738bc4c54ace178b81636810b5e9287f16a
Matheus Paiva <m.bacakhau@gmail.com>
2022-05-09 12:06:37 -0300

    fix: correção do bug no componente navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FirstComponentComponent } from './components/first-component/first-component.component';
-import { ParentsComponent } from './components/parents/parents.component';
-import { DirectComponent } from './components/direct/direct.component';
-import { RenderComponent } from './components/render/render.component';
-import { EventosComponent } from './components/eventos/eventos.component';
-import { InlockComponent } from './components/inlock/inlock.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent,
-    ParentsComponent,
-    DirectComponent,
-    RenderComponent,
-    EventosComponent,
-    InlockComponent
+    NavbarComponent,
+    
+  
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
