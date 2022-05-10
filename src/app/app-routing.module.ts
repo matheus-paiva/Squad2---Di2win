382d39c67e31cf4c89806dbc3e1bd16b3193f2d9
Matheus Paiva <m.bacakhau@gmail.com>
2022-05-10 17:05:55 -0300

    refactor: atualização de exibição do contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ContactComponent } from "./components/contact/contact.component";
 import { HomeComponent } from "./components/pages/home/home.component";
 import { SobreComponent } from "./components/pages/sobre/sobre.component";
 
 const routes: Routes = [
     {path: '', component: HomeComponent },
-    {path: 'Sobre', component: SobreComponent },
+    {path: 'Sobre', component: SobreComponent},
 ];
 
 @NgModule ({
