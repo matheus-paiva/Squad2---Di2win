30e4ec17f050a76bbb0188bd432ca18a23b9feb3
Matheus Paiva <m.bacakhau@gmail.com>
2022-05-24 14:07:42 -0300

    feat: nova arquitetura

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { LoginComponent } from "./components/login/login.component";
-import { CadastroComponent } from "./components/pages/cadastro/cadastro.component";
-import { HomeComponent } from "./components/pages/home/home.component";
-import { SobreComponent } from "./components/pages/sobre/sobre.component";
+import { LoginComponent } from "./Pages/login/login.component";
+import { CadastroComponent } from "./Pages/cadastro/cadastro.component";
+import { HomeComponent } from "./Pages/home/home.component";
+import { SobreComponent } from "./Pages/sobre/sobre.component";
+import { TrocarSenhaComponent } from "./Pages/trocar-senha/trocar-senha.component";
 
 const routes: Routes = [
     {path: '', component: HomeComponent },
     {path: 'Sobre', component: SobreComponent},
     {path: 'Login', component: LoginComponent},
     {path:'Cadastro',component: CadastroComponent},
+    {path:'usernamerecovery',component: TrocarSenhaComponent},
 ];
 
 @NgModule ({
