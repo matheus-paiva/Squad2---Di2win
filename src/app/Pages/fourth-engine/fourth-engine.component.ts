490c2150bc56adf1b92853460572ddaaae3907c1
Pedro Chalegre <pedrochalegre7@gmail.com>
2022-05-24 16:51:14 -0300

    feat(added fourth-engine screen): added fourth-engine screen. Already responsive

---
+++ src/app/Pages/fourth-engine/fourth-engine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fourth-engine',
+  templateUrl: './fourth-engine.component.html',
+  styleUrls: ['./fourth-engine.component.css']
+})
+export class FourthEngineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
