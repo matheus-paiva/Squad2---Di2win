f3b7dcb2cd734bb4185748442328b300d25c0120
Pedro Chalegre <pedrochalegre7@gmail.com>
2022-05-24 15:59:49 -0300

    feat(added third-engine screen): added screen and fixed some responsive bugs

---
+++ src/app/Pages/third-engine/third-engine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-third-engine',
+  templateUrl: './third-engine.component.html',
+  styleUrls: ['./third-engine.component.css']
+})
+export class ThirdEngineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
