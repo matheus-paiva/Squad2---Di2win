411df52614ba70ca89386869158170f13d5c71a9
Pedro Chalegre <pedrochalegre7@gmail.com>
2022-06-02 18:16:33 -0300

    feat(reworked navbar and added clickoutside directive): navbar already responsive and 100% done

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -10,17 +10,32 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   
 
+ 
   constructor() { 
-     this.toggleMenu();
+    this.toggleMenu();
+    this.toggleDropdown();
+  }
+
+  toggleMenu() {
+    const nav = document.getElementById('navbar');
+    nav?.classList.toggle('active');
+    nav?.classList.remove('drop');
+  }
+
+  toggleDropdown(){
+    const nav = document.getElementById('navbar');
+    nav?.classList.toggle('drop');
+  }
+
+  clickedOutside(){
+    const nav = document.getElementById('navbar');
+    nav?.classList.remove('drop');
+    nav?.classList.remove('active');
+  }
+
+  ngOnInit(): void {
     
-    }
-      toggleMenu() {
-      const nav = document.getElementById('navbar');
-      nav?.classList.toggle('active');
-    }
-    
-    ngOnInit(): void {
-      
-    }
-    
+  }
+  
+  
 }
