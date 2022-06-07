aa7db3b03b1f462a8e14cdd76fa2dccdb3df4928
Pedro Chalegre <pedrochalegre7@gmail.com>
2022-06-07 14:29:18 -0300

    fix(contact): added masks and validators on contact's inputs

--- src/app/Pages/sobre/sobre.module.ts
+++ src/app/Pages/sobre/sobre.module.ts
@@ -6,13 +6,18 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { SobreComponent } from './sobre.component';
 import { ContactComponent } from 'src/app/components/contact/contact.component';
 
+import { NgxMaskModule } from 'ngx-mask';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [SobreComponent,ContactComponent],
   imports: [
     CommonModule,
     SobreRoutingModule,
-    SharedModule
+    SharedModule,
+    NgxMaskModule.forRoot(),
+    ReactiveFormsModule,
   ]
 })
 export class SobreModule { }
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  contactForm!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+      this.contactForm = new FormGroup({
+      id: new FormControl(''),
+      name: new FormControl('', [Validators.required, Validators.pattern(/^[a-z][a-z\s]*$/)]),
+      email: new FormControl('', [Validators.required, Validators.pattern(/^(\s+\S+\s*)*(?!\s).*$/), Validators.email]),
+      empresa: new FormControl('', [Validators.required, Validators.pattern(/^(\s+\S+\s*)*(?!\s).*$/)]),
+      tel: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.pattern(/^[0-9]\d*$/)]),
+      message: new FormControl('', [Validators.required]),
+    })
+  }
+
+  get name(){
+    return this.contactForm.get('name')!;
+  }
+  get email(){
+    return this.contactForm.get('email')!;
+  }
+  get tel(){
+    return this.contactForm.get('tel')!;
+  }
+  get empresa(){
+    return this.contactForm.get('empresa')!;
+  }
+  get message(){
+    return this.contactForm.get('message')!;
+  }
+
+  submit(){
+
+    if(this.contactForm.invalid){
+      return
+    }
+
+    console.log("Enviado!")
   }
 
 }
